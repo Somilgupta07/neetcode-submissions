dfcdd8c34fb86592aa60696b263d7555be59e589
Somil Gupta <guptasomil0567@gmail.com>
2026-08-23 20:15:18 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int expand(string s,int left,int right){
+        int cnt=0;
+        while(left>=0 && right<s.size() && s[left]==s[right]){
+            cnt++;
+            left--;
+            right++;
+        }
+        return cnt;
+    }
+    int countSubstrings(string s) {
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            ans+=expand(s,i,i);
+            ans+=expand(s,i,i+1);
+        }
+        return ans;
+    }
+};
