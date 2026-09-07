6f012f760bb1584facbc2a91804a645797ab42a4
Somil Gupta <guptasomil0567@gmail.com>
2026-09-07 12:02:22 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canEat(vector<int>& piles,int h,int k){
+        long long hrs=0;
+        for(int pile: piles){
+            hrs+=(pile+k-1)/k;
+
+            if(hrs>h)
+                return false;
+            
+        }
+        return true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(canEat(piles,h,mid)){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
